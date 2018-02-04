fb3d66f1ae68be832a1b32b4dbb1aa7f71845c22
Rakesh Raja <rakeshraja@Rakeshs-MacBook-Air.local>
2018-02-04 16:27:09 -0500

    add fix to receive JSON data

--- post-test/express-post.js
+++ post-test/express-post.js
@@ -5,14 +5,16 @@ const bodyParser = require('body-parser')
 const app = express()
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Route that receives a POST request to /post
 app.post('/post', function (req, res) {
-  const body = req.body.Body
-  res.set('Content-Type', 'text/plain')
+  const body = req.body
+  res.set('Content-Type', 'application/json')
   console.log(body)
 })
+
 
 // Tell our app to listen on port 3001
 app.listen(3001, function (err) {
