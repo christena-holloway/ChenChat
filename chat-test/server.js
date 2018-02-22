7ecd2e4d5218ffd72305e7546d0bcfbc73fbd1e5
Rakesh Raja <rakeshraja@0587356368.wireless.umich.net>
2018-02-21 19:17:13 -0500

    commented out session code

--- chat-test/server.js
+++ chat-test/server.js
@@ -9,24 +9,35 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var port = process.env.PORT || 3000;
 var url = "https://chenchat2.azurewebsites.net";
 //need this so that all data can be sent to db correctly
+/*
 var session = require('express-session');
-var sess = {
-  secret: 'keyboard cat',
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(session({
+  key: 'user_sid',
+  secret: 'secretkey',
   resave: false,
   saveUninitialized: true,
   cookie: {},
   user_name: ''
-};
+}));
 
 if (app.get('env') === 'production') {
   app.set('trust proxy', 1) // trust first proxy
   sess.cookie.secure = true // serve secure cookies
-};
+};*/
 
-app.use(session(sess));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
 
 //Authentication code
 var GoogleAuth = require('google-auth-library');
@@ -147,7 +158,7 @@ function sendMessage(msg) {
     }
     console.log('%s corresponds to %s.', user.userID, user.fullName);
     username = user.fullName;
-    io.emit('chat message', (sess.name + ': ' + msg));
+    io.emit('chat message', (username + ': ' + msg));
   });
 
 }
@@ -208,10 +219,9 @@ var userSchema = new mongoose.Schema({
 }, {collection: "users"});
 
 var User = mongoose.model("User", userSchema);
-function sendUserInfo(token) {
+function sendUserInfo(token,req) {
   sub = getUID(token);
-  //var name = getName(token);
-  sess.name = getName(token);
+  var name = getName(token);
   User.count({ userID: sub }, function(err, count) {
     if (count === 0) {
       var u = new User({ 'userID': sub, 'fullName': name });
