6c28c0c5402820378d67c1ee6edb7297c2db7363
EmilyMacQuarrie <emilymac@umich.edu>
2018-04-06 13:02:57 -0400

    js form clear

--- chat-test/public/js/chat.js
+++ chat-test/public/js/chat.js
@@ -51,7 +51,7 @@ function addMembers() {
   socket.emit('entered emails', emails);
   //document.forms['mememail'].reset();
   window.location.href = '#close';
-  document.getElementById('mememail').reset();
+  document.getElementById('chat_mems').value = "";
 }
 
 function onLoad() {
