5ccfc393701bfb84cab03f329a00f1cca96bc65d
EmilyMacQuarrie <emilymac@umich.edu>
2018-04-08 18:58:11 -0400

    member list

--- chat-test/public/js/chat.js
+++ chat-test/public/js/chat.js
@@ -39,18 +39,21 @@ function openMod() {
 
 function addMembers() {
   let emails = document.getElementById("chat_mems").value;
-  let stripped = emails.replace(/\s/g, "");
-  let memberArr = stripped.split(',');
   
-  for (i = 0; i < memberArr.length; i++) {
-    $('#members').append(memberArr[i]);
+  let stripped = emails.replace(/\s/g, "");
+  let splitArr = stripped.split(',');
+  let emailArr = splitArr.filter(item => item.trim() !== '');
+
+  for (i = 0; i < emailArr.length; i++) {
+    let memlist = $('<li>').append(emailArr[i]);
+    memlist = memlist.append($('<br>'));
+    $('#members').append(memlist);
   }
-  //let socket = io();
+
   socket.emit('chat name', chat_name);
   socket.emit('entered emails', emails);
-  //document.forms['mememail'].reset();
+
   window.location.href = '#close';
-  //document.getElementById('chat_mems').value = "";
 }
 
 function onLoad() {
